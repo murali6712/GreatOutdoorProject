b275116a69e3a58f0f823536756d18f577f8194a
Asus <Asus@LAPTOP-MFHJN0PE>
2020-09-23 13:57:16 +0530

    SpringBoot

---
+++ GreatOutdoor/src/main/java/com/capgemini/greatoutdoor/exception/CustomEntityException.java
@@ -0,0 +1,48 @@
+package com.capgemini.greatoutdoor.exception;
+
+import java.util.Date;
+
+public class CustomEntityException {
+	
+	private Date timeStamp;
+	private String message;
+	private String errormsg;
+	
+	
+	public Date getTimeStamp() {
+		return timeStamp;
+	}
+	public void setTimeStamp(Date timeStamp) {
+		this.timeStamp = timeStamp;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public String getErrormsg() {
+		return errormsg;
+	}
+	public void setErrormsg(String errormsg) {
+		this.errormsg = errormsg;
+	}
+	
+	
+	
+	public CustomEntityException(Date timeStamp, String message, String errormsg) {
+		super();
+		this.timeStamp = timeStamp;
+		this.message = message;
+		this.errormsg = errormsg;
+	}
+	
+	
+	
+	public CustomEntityException() {
+		super();
+	}
+	
+	
+	
+}
--- GreatOutdoor/src/main/java/com/capgemini/greatoutdoor/exception/GlobelExceptionHandler.java
+++ GreatOutdoor/src/main/java/com/capgemini/greatoutdoor/exception/GlobelExceptionHandler.java
@@ -1,18 +1,21 @@
 package com.capgemini.greatoutdoor.exception;
 
+import java.util.Date;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 
 @RestControllerAdvice
 public class GlobelExceptionHandler {
 
 	@ExceptionHandler(value = ProductException.class)
-	public ResponseEntity<Object> exception(ProductException productException)
+	public ResponseEntity<Object> exception(ProductException productException, WebRequest webRequest)
 	{
-		
-		return new ResponseEntity<>(productException.getMessage(), HttpStatus.NOT_FOUND);
+		CustomEntityException error = new CustomEntityException(new Date(), productException.getMessage(), webRequest.getDescription(false));
+		return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
 		
 	}
 	
